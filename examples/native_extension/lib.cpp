8adb8405a7582b478b3369429b1d3a84d03936df
Dustin Van Tate Testa <toast27@gmail.com>
2019-04-04 12:03:40 -0500

    native extensions :D

---
+++ examples/native_extension/lib.cpp
@@ -0,0 +1,13 @@
+#include "../namespace_def.hpp"
+#include "../frame.hpp"
+
+// $ g++ lib.cpp ../namespace_def.cpp ../value.cpp -lgmp -lgmpxx -fPIC -shared -o lib.so
+
+Exit greet(Frame& f) {
+	std::cout <<"ff\n";
+	return Exit();
+}
+
+Value exports = Namespace({
+	{ "hi", Def(greet) }
+});
